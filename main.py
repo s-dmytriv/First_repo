f787778cb30c68d576eb712b8b3c53b2c0f8e8e7
s-dmytriv <solomiia.dmytriv@gmail.com>
2024-11-28 17:09:01 +0100

    update 28 November

--- main.py
+++ main.py
@@ -1,6 +1,75 @@
-num = 15  # приклад значення для num
+def factorial(n):
+    print("Виклик функції factorial з n = ", n)
+    if n == 1:
+        print("Базовий випадок, n = 1, повернення 1")
+        return 1
+    else:
+        result = n * factorial(n-1)
+        print("Повернення результату для n = ", n, ": ", result)
+        return result
 
-if num > 10:
-    print("num більше за 10")
-else:
-    print("num не більше за 10")
+print(factorial(5))
+
+import datetime
+now = datetime.datetime.now()
+print(now)
+
+from datetime import datetime
+
+# Створення двох об'єктів datetime
+datetime1 = datetime(2023, 3, 14, 12, 0)
+datetime2 = datetime(2023, 3, 15, 12, 0)
+
+# Порівняння дат
+print(datetime1 == datetime2)  # False, тому що дати не однакові
+print(datetime1 != datetime2)  # True, тому що дати різні
+print(datetime1 < datetime2)   # True, тому що datetime1 передує datetime2
+print(datetime1 > datetime2)   # False, тому що datetime1 не наступає за datetime2
+
+
+import time
+
+# Записуємо час на початку виконання
+start_time = time.perf_counter()
+
+# Виконуємо якусь операцію
+for _ in range(1_000_000):
+    pass  # Просто проходить цикл мільйон разів
+
+# Записуємо час після виконання операції
+end_time = time.perf_counter()
+
+# Розраховуємо та виводимо час виконання
+execution_time = end_time - start_time
+print(f"Час виконання: {execution_time} секунд")
+
+
+import random
+
+num = random.random()
+print(num)
+import random
+
+cards = ["Туз", "Король", "Дама", "Валет", "10", "9", "8", "7", "6"]
+
+random.shuffle(cards)
+
+
+
+one_line_text = "Textual data in Python is handled with str objects," \
+                " or strings. Strings are immutable sequences of Unicode" \
+                " code points. String literals are written in a variety " \
+                " of ways: single quotes, double quotes, triple quoted."
+print(one_line_text)
+
+url_search = "<https://www.google.com/search?q=Cat+and+dog&ie=utf-8&oe=utf-8&aq=t>"
+_, query = url_search.split('?')
+print(query)
+
+obj_query = {}
+for el in query.split('&'):
+    key, value = el.split('=')
+    obj_query.update({key: value.replace('+', ' ')})
+print(obj_query)
+
+
